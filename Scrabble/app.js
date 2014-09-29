580fbf05ba24ae89095d91e6733f0bdae78fa777
nwilder0 <nwilder0@yahoo.com>
2014-09-29 20:44:03 +0600

    Final code updates
    - Display class built and properly isolated
    - fixed scoring to match assignment guidelines
    - removed unused functions
    - removed quit button
    - still need:
    - help button displays nothing
    - test functions
    - cleanup of UI
    - update of class diagram

--- Scrabble/app.js
+++ Scrabble/app.js
@@ -1,91 +1,167 @@
+// global variables
+// each letter of the alphabet listed once
 var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// scrabble letter scores for each letter in order
 var letterScores = [1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10];
+// letters weighted by scrabble frequency - this is so we get vowels more often and Z and Q, not so much
 var lettersByFrequency = "AAAAAAAAABBCCDDDDEEEEEEEEEEEEFFGGGHHIIIIIIIIIJKLLLLMMNNNNNNOOOOOOOOPPQRRRRRRSSSSTTTTTTUUUUVVWWXYYZ";
-var numberOfLetters = 10;
-var filename = "words.txt";
+// number of letters to play
+var numberOfLetters = 7;
+// number of points to give if one gets all original dealt letters in one word
+var bonusScore = 50;
 
-function getRandomInt (min, max)
+// helper function to get a random integer in the range - not currently used
+function GetRandomInt (min, max)
 {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function getRandomMax (max) {
+// helper function to get random number between 0 and "max"
+function GetRandomMax (max) {
 	return Math.floor(Math.random() * max);
 }
 
-function Game () 
+// class Game - class diagram provided in diagram PDF - main class that controls game play
+function Game (display) 
 {
+	// instance of jQuery enabled display class
+	this.display = display;
+	
+	// the current hand of dealt letters
 	this.currentHand = Object.create(null);
 	
+	// associative array of letter scores by letter
 	this.letters = Object.create(null);
 	for(var i=0;i<alphabet.length;i++)
 	{
 		this.letters[alphabet.charAt(i)] = letterScores[i];
 	}
 	
-	this.DoCmd = function()
+	// function to handle user actions
+	this.DoCmd = function(strCmd, strParam)
 	{
-		// todo when Display is done
-	}
-	
-	this.LoadWords = function(filename) 
-	{
-		var rawFile = new XMLHttpRequest();
-		rawFile.open("GET", filename, false);
-		rawFile.onreadystatechange = function ()
+		// triggered by replay button - restart the current hand 
+		if(strCmd == "replay")
 		{
-			if(rawFile.readyState === 4)
-			{
-				if(rawFile.status === 200 || rawFile.status == 0)
-				{
-					this.words = " " + rawFile.responseText.toUpperCase() + " ";
-				}
-			}
+			// resets without changing dealt letters
+			this.currentHand.ResetHand();
+			// clears display elements back to game start
+			this.display.SetGameScreen();
+			// display the dealt letters
+			this.display.UpdateLetters(this.currentHand.GetRemaining());
 		}
-		rawFile.send();
+		
+		// triggered by clicking the new button - restart a new game
+		else if(strCmd == "new")
+		{
+			// just reload the page
+			location.reload();
+		}
+		
+		// try to play the word entered in the guess box
+		else if(strCmd == "play-word")
+		{
+			// clear any old messages
+			this.display.SetMessage("");
+			// change guess name for clarity
+			var guessWord = strParam;
+			// ignore everything if no word was passed
+			if(guessWord != "")
+			{
+				// make sure the word exists in the dictionary list (words.js)
+				if(this.CheckWord(guessWord))
+				{
+					// try to score the word with the current hand - returns 1=remaining letters, 
+					// 0=no more letters, -1=not enough letters for this word
+					var outcome = this.currentHand.ScoreWord(guessWord.toUpperCase(),this.GetWordScore(guessWord));
+					if(outcome >= 0)
+					{
+						// the word is good so add it to the display and update score and remaining letters
+						this.display.AddWordPlayed(guessWord,this.GetWordScore(guessWord));
+						this.display.UpdateScore(this.currentHand.GetScore());
+						this.display.UpdateLetters(this.currentHand.GetRemaining());
+					
+						// if no remaining letters then this hand is done
+						if(outcome == 0)
+						{
+							this.display.SetMessage("Hand is fully played.  Your score is " + this.currentHand.GetScore());
+							this.display.DisableGuess();
+						}
+					}
+					// not sufficient letters in hand to make this word
+					else
+					{
+						this.display.SetMessage("You don't have sufficient letters for this word");
+					}
+				}
+				// guessed word is not a valid word (based on the dictionary list)
+				else
+				{
+					this.display.SetMessage("Word not found in dictionary list");
+				}
+				// no matter what happened clear the guess box
+				this.display.ClearGuess();
+			}	
+		}
 	}
 	
+	// get the score for a specific letter
 	this.GetLetterScore = function(letter) 
 	{
 		return this.letters[letter.toUpperCase()];
 	}
 	
+	// get the word score for a full word
 	this.GetWordScore = function(tmpWord)
 	{
 		var tmpScore = 0;
 		
+		// add the letter scores for each letter in this word
 		for(var i = 0; i<tmpWord.length; i++)
 		{
 			tmpScore += this.GetLetterScore(tmpWord.charAt(i));
 		}
 		
+		// multiple the letter score sum by the word length
+		tmpScore *= tmpWord.length;
+		// if all original letters were used then add the bonus score
+		if(tmpWord.length == numberOfLetters)
+		{
+			tmpScore += bonusScore;
+		}
 		return tmpScore;
 	}
 	
+	// deal a new hand
 	this.NewHand = function()
 	{
+		// generate random set of letters and instantiate the new hand
 		this.currentHand = new Hand(this.GetRandomLetters());
+		// set all the display elements to their start values/configurations
+		this.display.SetGameScreen();
+		// show the random letters that were dealt
+		this.display.UpdateLetters(this.currentHand.GetRemaining());
 	}
 	
+	// deal out random letters
 	this.GetRandomLetters = function()
 	{
 		var randLetters = "";
+		// deal out a number of random letters determined by the global numberOfLetters 
+		//  and using the scrabble letter frequency
 		for( var i=0; i < numberOfLetters; i++ )
 		{	
-			randLetters += lettersByFrequency.charAt(getRandomMax(lettersByFrequency.length));
+			randLetters += lettersByFrequency.charAt(GetRandomMax(lettersByFrequency.length));
 		}
 		return randLetters;
 	}
 	
-	this.ReplayHand = function()
-	{
-		this.currentHand.ResetHand();
-	}
-	
+	// see if a word is in the dictionary list
 	this.CheckWord = function(word)
 	{
 		var wordFound = false;
 		
+		// search through the list which is just a giant string with words separated by spaces
 		var pos = words.indexOf(" " + word.toLowerCase() + " ");
 		if(pos >= 0)
 		{
@@ -94,41 +170,42 @@ function Game ()
 		return wordFound;
 	}
 	
-	this.EndGame = function()
-	{
-		// really needed?
-	}
 }
 
+// class Hand - represents a specific instance of the game with a specific set of dealt letters
 function Hand(dealtLetters)
 {
+	// the original dealt letters
 	this.dealtLetters = dealtLetters;
+	// the remaining letters (starts out the same as the original dealt ones)
 	this.remaining = dealtLetters;
 	this.score = 0;
 	
+	// resets the hand with the original letters
 	this.ResetHand = function() 
 	{
 		this.score = 0;
 		this.remaining = this.dealtLetters;
 	}
 
-	this.HasLetters = function(guess)
-	{
-		// probably not needed
-	}
-	
+	// tries to use the word (with score) that has been guessed and validated in the dictionary list
 	this.ScoreWord = function(guess, wordScore)
 	{
-
+		// assume sufficient letters in hand
 		var result = 1;
 
+		// temporary hand of letters representing the hand as each letter of the word is removed 
+		//  necessary to solve problem with multiples of the same letter in the word
 		var tmpRemaining = this.remaining;
 		
+		// for each letter in the guess
 		for(var i = 0; i<guess.length && result==1; i++)
 		{
+			// find if the letter remains in the temporary hand
 			var pos = tmpRemaining.indexOf(guess.charAt(i));
 			if(pos >= 0)
 			{
+				// if it remains remove it
 				if(pos < tmpRemaining.length-1) 
 				{
 					tmpRemaining = tmpRemaining.substr(0,pos) + tmpRemaining.substr(pos+1,tmpRemaining.length-1);
@@ -138,12 +215,15 @@ function Hand(dealtLetters)
 					tmpRemaining = tmpRemaining.substr(0,pos);
 				}
 			}
+			// if the letter doesn't remain in the temporary hand
+			//  then the guess is no good
 			else
 			{
 				result = -1;
 			}
 		}
 		
+		// if the guess was good then update the actual hand and the score
 		if(result > -1)
 		{
 			this.score += wordScore;
@@ -154,89 +234,116 @@ function Hand(dealtLetters)
 			}
 		}
 		
+		// return an integer code indicating the outcome:
+		// -1 = no good
+		// 0 = good and no letters remain in hand
+		// 1 = good and letters remain
 		return result;
 		
 	}
 	
+	// return the current score
 	this.GetScore = function() 
 	{
 		return this.score;
 	}
 	
+	// return the current remaining letters
 	this.GetRemaining = function()
 	{
 		return this.remaining;
 	}
 	
-	this.QuitHand = function()
-	{
-		// really needed?
-	}
 }
 
-
+// the jQuery base object
 $(function() {
 	
-	var game = new Game();
-	//game.LoadWords();
-	game.NewHand();
-	
-	$('#hLetters').val(game.currentHand.GetRemaining().split("").join(" "));
-	$('#chars').html($('#hLetters').val());
-	
-	$('#bReplay').on('click',function () {
-		
-		game.currentHand.ResetHand();
-		$('#words').html("");
-		$("#score").html(0);
-		$('#mesgArea').html("");
-		$('#bPlayWord').prop("disabled",false);
-		$('#txtGuess').prop("disabled",false);
-		$('#chars').html($('#hLetters').val());
-	
-	});
-	
-	function tryWord() {
-		var guessWord = $('#txtGuess').val();
-		if(guessWord != "")
+	// define the display class here so there are no issues accessing the jQuery objects
+	function Display() 
+	{
+		// update the letters in hand display element
+		this.UpdateLetters = function(strRemaining)
 		{
-			if(game.CheckWord(guessWord))
-			{
-				var outcome = game.currentHand.ScoreWord(guessWord.toUpperCase(),game.GetWordScore(guessWord));
-				if(outcome >= 0)
-				{
-					$('#words').html($('#words').html() + " " + guessWord);
-					$("#score").html(game.currentHand.GetScore());
-					$('#chars').html(game.currentHand.GetRemaining().split("").join(" "));
-					
-					if(outcome == 0)
-					{
-						$('#mesgArea').html("Hand is fully played.  Your score is " + $('#score').html());
-						$('#bPlayWord').prop("disabled",true);
-						$('#txtGuess').prop("disabled",true);
-					}
-				}
-				else
-				{
-					$('#mesgArea').html("You don't have sufficient letters for this word");
-				}
-			}
-			else
-			{
-				$('#mesgArea').html("Word not found in dictionary list");
-			}
+			$('#chars').html(strRemaining.split("").join(" "));
+		}
+		
+		// update the sore display element
+		this.UpdateScore = function(intScore)
+		{
+			$('#score').html(intScore);
+		}
+		
+		// update the played words display element
+		this.AddWordPlayed = function(strGuessWord,intWordScore)
+		{
+			$('#words').html($('#words').html() + " " + strGuessWord + "(" + intWordScore + ")");
+		}
+		
+		// clear the guess box
+		this.ClearGuess = function()
+		{
 			$('#txtGuess').val("");
 		}
-	
+		
+		// set a message in the message display element
+		this.SetMessage = function(strMesg)
+		{
+			$('#mesgArea').html(strMesg);
+		}
+		
+		// disable the guess box and play word button - used when all letters have been played
+		this.DisableGuess = function()
+		{
+			$('#bPlayWord').prop("disabled",true);
+			$('#txtGuess').prop("disabled",true);			
+		}
+
+		// resets all the display elements to their original states
+		this.SetGameScreen = function() 
+		{
+			$('#words').html("");
+			$('#mesgArea').html("");
+			$('#bPlayWord').prop("disabled",false);
+			$('#txtGuess').prop("disabled",false);
+			this.UpdateScore(0);
+		}
+				
 	}
 	
-	$('#bPlayWord').on('click', tryWord);
+	// the actual instance of the display class
+	var display = new Display();
+
+	// the actual instance of the game class
+	var game = new Game(display);
+	// generate a new hand to start the game
+	game.NewHand();
 	
+	// set the replay button to the appropriate game command
+	$('#bReplay').on('click',function() 
+	{
+		game.DoCmd("replay","");
+	});
+	
+	// set the new button to the appropriate game command
+	$('#bNew').on('click',function()
+	{
+		game.DoCmd("new","");
+	});
+		
+	// set the play button to the appropriate game command
+	$('#bPlayWord').on('click', function() 
+	{
+		game.DoCmd("play-word",$('#txtGuess').val());
+	});
+	
+	// set the enter key within the guess box to the appropriate game command
 	$('#txtGuess').on('keypress', function(e)
 	{
 		if(e.keyCode==13)
 		{
-			tryWord();
+			game.DoCmd("play-word",$('#txtGuess').val());
 		}
 	});
+	
 });
